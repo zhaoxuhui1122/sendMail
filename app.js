846e209252a80f8eedb83b72bf52880b071d99a1
zhaoxuhui1122 <1258835133@qq.com>
2017-06-15 14:14:09 +0800

    update

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ app.set("views", "./views"); //设置根目录
 app.set("view engine", "jade"); //设置默认模板
 app.use(bodyParser.json()); //body-parser
 app.use(bodyParser.urlencoded({extended: true}));
+
 //增加了跨域请求
 const jsonp = require("./config/jsonp");
 app.all("*",jsonp);
 //service
 const sendMail = require("./service/email"); //email
 //请求接口
-app.post("/sendMail", sendMail);
-app.get("/",function(req,res){
+
+app.post("/sendMail", sendMail);//发送邮件
+
+app.get("/",function(req,res){//首页
 	res.render("sendmail");
 })
 //启动服务
